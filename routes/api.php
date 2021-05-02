c75a0d671fb091aedc8aa5c46d8d27d16f613081
sopegue <yayasopeguesoro@gmail.com>
2021-05-03 00:09:38 +0100

    dfbd

--- routes/api.php
+++ routes/api.php
@@ -80,18 +80,3 @@ Route::post('client/existence', [ClientAuthController::class, 'isEmailFreeApi'])
 Route::post('agent/existence', [AgentAuthController::class, 'isEmailFreeApi']);
 
 Route::post('agence/existence', [AgenceController::class, 'isEmailFreeApi']);
-
-Route::get('properties/fav/{key}/{sort}', [PropertyController::class, 'favPropApi']);
-Route::get('properties/agfirst/{key}/{exclude}', [PropertyController::class, 'propAgCountApi']);
-Route::get('properties/ag/{key}/{exclude}', [PropertyController::class, 'propAgApi']);
-Route::get('properties/villesfirst/{key}/{id}', [PropertyController::class, 'propVilleFirstApi']);
-Route::get('properties/villes/{key}/{id}', [PropertyController::class, 'propVilleApi']);
-Route::get('properties/search/{key}', [PropertyController::class, 'searchKeyApi']);
-Route::post('properties/search', [PropertyController::class, 'searchApi']);
-Route::get('properties/bytype/{type}', [PropertyController::class, 'showByType']);
-Route::get('properties/bytype/skip/{type}', [PropertyController::class, 'showByTypeSkip']);
-Route::get('property/agent/{show}', [PropertyController::class, 'showOwn']);
-Route::get('property/{mark}/{as}/{id}', [PropertyController::class, 'soldOrRent']);
-Route::get('property/visit/{id}', [PropertyController::class, 'visitApi']);
-Route::post('property/update', [PropertyController::class, 'updateApi']);
-Route::post('properties/viewed', [PropertyController::class, 'viewedApi']);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Property\PropertyController;
+
+Route::get('properties/fav/{key}/{sort}', [PropertyController::class, 'favPropApi']);
+Route::get('properties/agfirst/{key}/{exclude}', [PropertyController::class, 'propAgCountApi']);
+Route::get('properties/ag/{key}/{exclude}', [PropertyController::class, 'propAgApi']);
+Route::get('properties/villesfirst/{key}/{id}', [PropertyController::class, 'propVilleFirstApi']);
+Route::get('properties/villes/{key}/{id}', [PropertyController::class, 'propVilleApi']);
+Route::get('properties/search/{key}', [PropertyController::class, 'searchKeyApi']);
+Route::post('properties/search', [PropertyController::class, 'searchApi']);
+Route::get('properties/bytype/{type}', [PropertyController::class, 'showByType']);
+Route::get('properties/bytype/skip/{type}', [PropertyController::class, 'showByTypeSkip']);
+Route::get('property/agent/{show}', [PropertyController::class, 'showOwn']);
+Route::get('property/{mark}/{as}/{id}', [PropertyController::class, 'soldOrRent']);
+Route::get('property/visit/{id}', [PropertyController::class, 'visitApi']);
+Route::post('property/update', [PropertyController::class, 'updateApi']);
+Route::post('properties/viewed', [PropertyController::class, 'viewedApi']);
+
 
 /*
 |--------------------------------------------------------------------------
