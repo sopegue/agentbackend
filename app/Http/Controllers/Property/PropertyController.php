3fb68128104e2b6d8723aaf739485637a5ad84eb
sopegue <yayasopeguesoro@gmail.com>
2021-05-26 23:07:51 +0100

    dezd

--- app/Http/Controllers/Property/PropertyController.php
+++ app/Http/Controllers/Property/PropertyController.php
@@ -394,15 +394,16 @@ class PropertyController extends Controller
     {
         //
         $results = [];
-        $presearches = Adresse::Has('property')->where('adresse', 'like',  $key . '%')
-            ->orWhere('adresse', 'like',  '%' . $key . '%')
-            ->orWhere('adresse', 'like',  '%' . $key)
-            ->orWhere('ville', 'like',  $key . '%')
-            ->orWhere('ville', 'like',  '%' . $key . '%')
-            ->orWhere('ville', 'like',  '%' . $key)
-            ->orWhereIn('adresse', [$key])
-            ->orWhereIn('ville', [$key])
-            ->orderByDesc('created_at')
+        $presearches = Adresse::whereHas('property', function (Builder $query) use ($key) {
+            $query->where('adresse', 'like',  $key . '%');
+            $query->orWhere('adresse', 'like',  '%' . $key . '%');
+            $query->orWhere('adresse', 'like',  '%' . $key);
+            $query->orWhere('ville', 'like',  $key . '%');
+            $query->orWhere('ville', 'like',  '%' . $key . '%');
+            $query->orWhere('ville', 'like',  '%' . $key);
+            $query->orWhereIn('adresse', [$key]);
+            $query->orWhereIn('ville', [$key]);
+        })->orderByDesc('created_at')
             ->take(20)
             ->get();
         if (!$presearches->isEmpty()) {
